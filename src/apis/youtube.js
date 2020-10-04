f4d7aae9b87032a0afba19ebb54243a1cb68dc7d
Stewart Glover <stewart.glover191@gmail.com>
2020-10-04 14:17:26 +0100

    updated security

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-
-const KEY = 'AIzaSyDE5JN-Hi7GcGWtud4gDWepOJxdXKV95XQ';
+const KEY = process.env.REACT_APP_API_KEY
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
